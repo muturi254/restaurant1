070163c3b1eb6b92d3218c392324a6fcf7a8affb
muturi254 <petermuturi74@gmail.com>
2021-05-24 16:54:34 +0300

    add fragment

---
+++ app/src/main/java/com/example/restaurants/ui/RestaurantDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.restaurants.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restaurants.R;
+
+public class RestaurantDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+    }
+}
